fbb8c49f5c84ac0dba27a6a2c84eb911068993e8
Joshua Liu <josh.k.liu@gmail.com>
2014-02-05 22:43:54 -0800

    Added node.js, reorganized directories (required by node)

---
+++ app.js
@@ -0,0 +1,39 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+var app = express();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.engine('html', require('ejs').renderFile);
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/chat', routes.chat);
+app.get('/inventory', routes.inventory);
+app.get('/', routes.index);
+
+app.listen(3000, function(){
+  console.log("Express server listening on port 3000 in %s mode", app.settings.env);
+});
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+
+exports.index = function (req, res) {
+	res.render('index.html');
+}
+
+exports.inventory = function(req, res) {
+	res.render('inventory.html');
+}
+
+exports.chat = function(req, res) {
+	res.render('chat.html');
+}
